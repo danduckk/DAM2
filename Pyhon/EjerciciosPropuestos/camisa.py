684f38dc7bef8e29179c42c1ae5b4216fb4ceca7
daniel <daniselos3@gmail.com>
2026-09-18 13:14:29 +0200

    hola aaaaa

---
+++ Pyhon/EjerciciosPropuestos/camisa.py
@@ -0,0 +1,13 @@
+precioCamisa = 35
+dinero = 50
+
+precioFinal = precioCamisa - (precioCamisa * 0.10)
+
+dineroFinal = dinero - precioFinal
+
+
+print("precio final: ", precioFinal)
+print("te quedan ", dineroFinal, "euros")
+
+for i in range(9):
+    print("hola")
---
+++ Pyhon/EjerciciosPropuestos/cocheGastaLitros.py
@@ -0,0 +1,9 @@
+litros = 5.5/100
+distancia = 15
+distanciaRecorrida = 600
+precioGasolina = 1.12
+
+litrosTotal = distanciaRecorrida * litros
+costeTotal = litrosTotal * precioGasolina
+
+print("Te gastas ", costeTotal)
